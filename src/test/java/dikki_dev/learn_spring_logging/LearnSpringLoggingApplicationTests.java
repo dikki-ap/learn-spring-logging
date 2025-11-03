378652b87786c176067e52803adac3a76b0df422
Dikki AP <dikki.agung@epson.co.id>
2025-11-03 11:05:10 +0700

    Initial

---
+++ src/test/java/dikki_dev/learn_spring_logging/LearnSpringLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package dikki_dev.learn_spring_logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
